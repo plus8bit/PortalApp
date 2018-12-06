785846c015c1858966135ac65ea491462e3c1d99
Nick <plus0bit@gmail.com>
2018-12-06 22:46:38 +0300

    Add partial file _form.html.erb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,4 +8,23 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+
+    if @blog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
 end
